4cf23c1bd35353b48635224917c0f938721f7b1c
Илья Топоров <ya.toporow@yandex.ru>
2025-04-14 20:56:30 +0300

    added domain layer

---
+++ src/CorporateSystem.SharedDocsMicroservice/CorporateSystem.SharedDocs.Domain/Entities/Document.cs
@@ -0,0 +1,11 @@
+﻿namespace CorporateSystem.SharedDocs.Domain.Entities;
+
+public class Document
+{
+    public int Id { get; init; }
+    public int OwnerId { get; init; }
+    public required string Title { get; init; }
+    public string Content { get; init; } = string.Empty;
+    public required DateTimeOffset? ModifiedAt { get; init; }
+    public required DateTimeOffset CreatedAt { get; init; }
+}
---
+++ src/CorporateSystem.SharedDocsMicroservice/CorporateSystem.SharedDocs.Domain/Entities/DocumentUser.cs
@@ -0,0 +1,11 @@
+﻿using CorporateSystem.SharedDocs.Domain.Enums;
+
+namespace CorporateSystem.SharedDocs.Domain.Entities;
+
+public class DocumentUser
+{
+    public int Id { get; init; }
+    public int DocumentId { get; init; }
+    public int UserId { get; init; }
+    public AccessLevel AccessLevel { get; init; }
+}
