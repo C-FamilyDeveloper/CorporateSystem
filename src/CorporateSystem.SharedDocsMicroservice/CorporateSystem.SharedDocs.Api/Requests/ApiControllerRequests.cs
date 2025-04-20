d695bf26f33751bb31c80bd1a1ffdf711e39cce5
Илья Топоров <ya.toporow@yandex.ru>
2025-04-20 16:08:41 +0300

    added new route

--- src/CorporateSystem.SharedDocsMicroservice/CorporateSystem.SharedDocs.Api/Requests/ApiControllerRequests.cs
+++ src/CorporateSystem.SharedDocsMicroservice/CorporateSystem.SharedDocs.Api/Requests/ApiControllerRequests.cs
@@ -30,3 +30,21 @@ public class GetDocumentsResponse
     [JsonPropertyName("title")]
     public required string Title { get; init; }
 }
+
+public class CreateDocumentRequest
+{
+    [JsonPropertyName("title")]
+    public required string Title { get; init; }
+}
+
+public class CreateDocumentResponse
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("title")]
+    public required string Title { get; set; }
+    
+    [JsonPropertyName("content")]
+    public required string Content { get; set; }
+}
