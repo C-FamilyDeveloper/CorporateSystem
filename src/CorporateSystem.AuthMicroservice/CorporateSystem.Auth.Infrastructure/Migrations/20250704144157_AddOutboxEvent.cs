af67cc7fc48847f1247d68301fa9f93583bb7a69
Илья Топоров <ya.toporow@yandex.ru>
2025-07-04 19:47:34 +0300

    added OutboxEvent + migrations

---
+++ src/CorporateSystem.AuthMicroservice/CorporateSystem.Auth.Infrastructure/Migrations/20250704144157_AddOutboxEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CorporateSystem.Auth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOutboxEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EventType = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Payload = table.Column<string>(type: "text", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxEvent_Processed_CreatedAt",
+                table: "OutboxEvents",
+                columns: new[] { "Processed", "CreatedAtUtc" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxEvents");
+        }
+    }
+}
